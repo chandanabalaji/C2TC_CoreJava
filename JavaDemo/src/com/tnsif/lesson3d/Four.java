d3fcd214da89f4062040d963c48b7295270d1701
Chandana B <Chandana.B@DESKTOP-QUCRHUO>
2022-08-24 21:43:14 +0530

    second commit

--- JavaDemo/src/com/tnsif/lesson3d/Four.java
+++ JavaDemo/src/com/tnsif/lesson3d/Four.java
@@ -1,10 +1,26 @@
+// program to demonstrate import package.*;
+
 package com.tnsif.lesson3d;
+import com.tnsif.lesson3a.One;
+import com.tnsif.lesson3b.*;
+import com.tnsif.lesson3c.Three;
 
 public class Four {
+
+	void dispOutput()
+	{
+		System.out.println("I am Four");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Two obj = new Two();	
+		obj.dispOutput();
+		Three obj1 = new Three();
+		obj1.dispOutput();
+		One obj2 = new One();
+		obj2.dispOutput();
 	}
 
 }
--- JavaDemo/src/com/tnsif/lesson3e/Five.java
+++ JavaDemo/src/com/tnsif/lesson3e/Five.java
@@ -1,10 +1,13 @@
 package com.tnsif.lesson3e;
-
+import com.tnsif.lesson3f.*;
 public class Five {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Six obj = new Six();
+		obj.dispOutput();
+		
 	}
 
 }
